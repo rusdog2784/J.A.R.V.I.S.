f733bd942c5bb96748fd463c463b3364ae34b32d
Scott Russell <scott.tkdmaster@mac.com>
2018-07-10 16:11:55 -0400

    Commit from iMac

--- actions.py
+++ actions.py
@@ -27,6 +27,8 @@ class Actions:
     applications = []
     movieNames = []
     folders = {}
+    news_api_key = ""
+    spotify_token = ""
 
     # Initializing function that is called at the start of JARVIS.
     def __init__(self, location):
@@ -42,26 +44,26 @@ class Actions:
         if not creds or creds.invalid:
             flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
             creds = tools.run_flow(flow, store)
-        service = build('calendar', 'v3', http=creds.authorize(Http()))
+        self.service = build('calendar', 'v3', http=creds.authorize(Http()))
         # Initiating website variable from the "websites.json" file which contains a dictionary of websites
         with open("websites.json", 'r') as f:
-            websites = json.load(f)
+            self.websites = json.load(f)
         # Initiating applications variable from the "applications.txt" file which contains all system applications
         with open("applications.txt", "r") as f:
-            applications = f.readlines()
-        applications = [x.strip() for x in applications]
+            self.applications = f.readlines()
+        self.applications = [x.strip() for x in self.applications]
         # Initiating marvel movie variable, marvelMovies, from the "marvel_movies.txt" file.
         with open('marvel_movies.txt', 'r') as f:
-                movieNames = f.readlines()
+                self.movieNames = f.readlines()
         # Spotify Authentication
         username = 'scott.tkdmaster@mac.com'
         scope = 'user-library-read user-read-private streaming'
-        token = util.prompt_for_user_token(username, scope)
+        self.spotify_token = util.prompt_for_user_token(username, scope)
         # Initializing News API
-        news_api_key = '603fdd9223614b60826a80b8ad29afa8'
+        self.news_api_key = '603fdd9223614b60826a80b8ad29afa8'
         # Initiating folders variable from the "folders.json" file which contains a dictionary of folder locations
         with open("folders.json", 'r') as f:
-            folders = json.load(f)
+            self.folders = json.load(f)
 
     # Function to gather weather data for the specified location
     # Returns object (city, condition, high, low)
@@ -104,7 +106,7 @@ class Actions:
     # few. 
     def getNews(self):
         # Creating News API object
-        newsapi = NewsApiClient(api_key=api_key)
+        newsapi = NewsApiClient(api_key=self.news_api_key)
         top_headlines = newsapi.get_top_headlines(sources='bbc-news,the-verge,the-wall-street-journal,bloomberg,the-economist,wired')
         articles = top_headlines['articles']
         if len(articles) > 10:
@@ -147,12 +149,12 @@ class Actions:
         return
 
     def openFolder(self, location):
-        if len(folders) == 0:
+        if len(self.folders) == 0:
             print("Error, something happened with loading folder locations. The object, folders, is empty.")
             return
         location.lower()
-        if location in folders:
-            command = '''open "%s"''' % (folders[location])
+        if location in self.folders:
+            command = '''open "%s"''' % (self.folders[location])
             os.system(command)
         else:
             print("Folder not logged or doesn't exist.")
@@ -161,12 +163,12 @@ class Actions:
     # Function to open the specified website in a web browser assuming it knows
     # what website the user is talking about.
     def goToWebsite(self, location):
-        if len(websites) == 0:
+        if len(self.websites) == 0:
             print("Error, something happened with loading website locations. The object, websites, is empty.")
             return
         location = location.lower()
-        if location in websites:
-            webbrowser.open_new_tab(websites[location])
+        if location in self.websites:
+            webbrowser.open_new_tab(self.websites[location])
         else:
             print("Folder not logged or doesn't exist.")
         return "Opening " + location
@@ -174,10 +176,10 @@ class Actions:
     # Function will play the specified Marvel movie from iTunes.
     def playMarvelMovie(self, title):
         if title == None or title == "":
-            if len(movieNames) == 0:
+            if len(self.movieNames) == 0:
                 movieName = "Iron Man"
             else:
-                movieName = movieNames[random.randint(0, len(movieNames - 1))]
+                movieName = self.movieNames[random.randint(0, len(self.movieNames - 1))]
         else:
             movieName = title
         osascript = '''
@@ -207,11 +209,11 @@ class Actions:
     # Function opens an application specified assuming it is an application present
     # on the local machine and inside the Applications folder.
     def openApplication(self, application):
-        if len(applications) == 0:
+        if len(self.applications) == 0:
             print("Error, something happened with loading website locations. The object, applications, is empty.")
             return
         application = application + ".app"
-        if application in applications:
+        if application in self.applications:
             os.system("open -a '" + application + "'")
         else:
             return "Invalid Application Specified."
@@ -220,24 +222,23 @@ class Actions:
     # Function quits out of the application specified assuming it is an application
     # present on the local machine and inside the Applications folder.
     def closeApplication(self, application):
-        if len(applications) == 0:
+        if len(self.applications) == 0:
             print("Error, something happened with loading website locations. The object, applications, is empty.")
             return
         application = application + ".app"
-        if application in applications:
+        if application in self.applications:
             command = """osascript -e 'quit app "%s"'""" % (application)
             os.system(command)
         else:
             return "Invalid Application Specified."
         return application + " opened." 
-        return
 
     # Function created a calendar event in my personal google calendar with a
     # specified title, start date, and end date. If end date is not specified,
     # then end becomes the start date plus 30 minutes by default. This function
     # returns a string, success, with the calendar html link.
     def createCalendarEvent(self, title, start, end):
-        if service == None:
+        if self.service == None:
             print("Error, something happened with creating a calendar event. The object, service, has not been created.")
             return
         if end == None:
@@ -257,7 +258,7 @@ class Actions:
                 'useDefault': True
             }
         }
-        newEvent = service.events().insert(calendarId='primary', body=eventData).execute()
+        newEvent = self.service.events().insert(calendarId='primary', body=eventData).execute()
         success = 'Event created: %s' % (newEvent.get('htmlLink'))
         return success
     
@@ -267,8 +268,8 @@ class Actions:
     # performs a search based on the search and type, returns the first item,
     # then, using applescript, plays the desired spotify uri.
     def playSpotify(self, search, type):
-        if token:
-            sp = spotipy.Spotify(auth=token)    
+        if self.spotify_token:
+            sp = spotipy.Spotify(auth=self.spotify_token)    
             result = sp.search(q=search, type=type)
             if result:
                 if result[type+'s']:
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gtts import gTTS
 import os, time, random, csv, os.path
 from actions import Actions
 
+awake = False
 commandToSave = ""
 actionToSave = ""
 unknown_fieldnames = ['Command', 'Action']
@@ -15,7 +16,8 @@ def speak(audioString):
     tts.save("audio.mp3")
     os.system("mp321 audio.mp3")
     '''
-    command = "say '" + audioString + "'"
+    #command = "say '" + audioString + "'"
+    command = 'say "' + audioString + '"'
     os.system(command)
 
 def recordAudio():
@@ -53,7 +55,8 @@ def getRandomAdjective():
     return adj
 
 def jarvis(data):
-    if "hello" in data or "good morning" in data or "good afternoon" in data or "good evening" in data:
+    global awake
+    if "hello" in data or "good morning" in data or "good afternoon" in data or "good evening" in data or "hey" in data:
         tod = getTimeOfDay()
         if "how are you" in data:
             adjective = getRandomAdjective()
@@ -103,7 +106,9 @@ def jarvis(data):
         return
     if "goodbye" in data:
         speak("Goodbye")
-        exit()
+        awake = False
+        #exit()
+        return
     if data == "":
         return
     speak("Command unknown. Save command?")
@@ -125,6 +130,12 @@ if os.path.isfile('unknown_commands.csv') == False:
 time.sleep(2)
 while 1:
     data = recordAudio()
-    if "JARVIS" in data:
+    if "goodbye" in data:
         jarvis(data)
+    if awake == True:
+        jarvis(data)
+    else:
+        if "Jarvis" in data:
+            awake = True
+            jarvis(data)
     print
